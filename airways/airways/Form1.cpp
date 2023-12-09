b6425ef62f5a33cb8a3f563bc1494e685679f0aa
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-12-09 18:17:20 +0100

    input baggage interface test

--- airways/airways/Form1.cpp
+++ airways/airways/Form1.cpp
@@ -2,15 +2,17 @@
 #include "Form1.h"
 #include "Baggage.h"
 #include "SmallBag.h"
+#include "HandLuggage.h"
 // #include "Form1.h"
 
 System::Void CppCLRWinFormsProject::Form1::button1_Click(System::Object^ sender, System::EventArgs^ e)
 {
-	Baggage* smallBagObject = new SmallBag(10.0, { 1, 2, 3 }, true);
+    Baggage* bag = new HandLuggage(System::Convert::ToDouble(this->textBox4->Text), { System::Convert::ToInt32(this->textBox1->Text),System::Convert::ToInt32(this->textBox2->Text),System::Convert::ToInt32(this->textBox3->Text) });
+    this->textBox1->Text = "";
+    this->textBox2->Text = ""; 
+    this->textBox3->Text = "";
+    this->textBox4->Text = "";
+    String^ bagInfo = gcnew String(bag->display().c_str());
 
-	this->label1->Text = gcnew String(smallBagObject->display().c_str());
-
-	delete smallBagObject;
-
-
+    this->listBox1->Items->Add(bagInfo);
 }
--- airways/airways/Form1.h
+++ airways/airways/Form1.h
@@ -34,9 +34,19 @@ namespace CppCLRWinFormsProject {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
-	protected:
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	protected:
+
+	protected:
+
 
 
 
@@ -58,37 +68,110 @@ namespace CppCLRWinFormsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Cursor = System::Windows::Forms::Cursors::Default;
-			this->button1->Location = System::Drawing::Point(93, 79);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(256, 222);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label1->Location = System::Drawing::Point(12, 9);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
-			this->label1->TabIndex = 1;
-			this->label1->Text = L"label1";
+			this->label1->Size = System::Drawing::Size(126, 20);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Input baggage";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label2->Location = System::Drawing::Point(7, 44);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(35, 18);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"size";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(62, 45);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(34, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(104, 45);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(34, 20);
+			this->textBox2->TabIndex = 3;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(144, 44);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(34, 20);
+			this->textBox3->TabIndex = 4;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(63, 76);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(34, 20);
+			this->textBox4->TabIndex = 5;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label4->Location = System::Drawing::Point(7, 75);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(50, 18);
+			this->label4->TabIndex = 7;
+			this->label4->Text = L"weight";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(14, 114);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(82, 20);
+			this->button1->TabIndex = 8;
+			this->button1->Text = L"Add";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(214, 12);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(562, 654);
+			this->listBox1->TabIndex = 9;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(800, 800);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->listBox1);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->ResumeLayout(false);
@@ -104,7 +187,8 @@ namespace CppCLRWinFormsProject {
 	
 	
 	private:
-		System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
-	
-	};
+		
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
+
+};
 }
