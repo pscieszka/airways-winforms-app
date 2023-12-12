af9987304762bb718063ead3d0a07bb0420ece48
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-12-12 21:50:31 +0100

    background

--- airways/airways/Form1.h
+++ airways/airways/Form1.h
@@ -38,7 +38,14 @@ namespace airways {
 		}
 	private: System::Windows::Forms::Panel^ panelMenu;
 	private: System::Windows::Forms::Panel^ panelProjName;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+	private: System::Windows::Forms::Button^ buttonNewFlight;
+	private: System::Windows::Forms::Button^ buttonTbd;
+	private: System::Windows::Forms::Button^ buttonShowFlights;
+
+	private: System::Windows::Forms::Button^ buttonHome;
+	private: System::Windows::Forms::PictureBox^ pictureBoxBackground;
+
 
 
 	protected:
@@ -96,17 +103,24 @@ namespace airways {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
 			this->panelMenu = (gcnew System::Windows::Forms::Panel());
+			this->buttonTbd = (gcnew System::Windows::Forms::Button());
+			this->buttonShowFlights = (gcnew System::Windows::Forms::Button());
+			this->buttonNewFlight = (gcnew System::Windows::Forms::Button());
 			this->panelProjName = (gcnew System::Windows::Forms::Panel());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->buttonHome = (gcnew System::Windows::Forms::Button());
+			this->pictureBoxBackground = (gcnew System::Windows::Forms::PictureBox());
 			this->panelMenu->SuspendLayout();
 			this->panelProjName->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxBackground))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// panelMenu
 			// 
 			this->panelMenu->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(51)), static_cast<System::Int32>(static_cast<System::Byte>(51)),
 				static_cast<System::Int32>(static_cast<System::Byte>(51)));
+			this->panelMenu->Controls->Add(this->buttonTbd);
+			this->panelMenu->Controls->Add(this->buttonShowFlights);
+			this->panelMenu->Controls->Add(this->buttonNewFlight);
 			this->panelMenu->Controls->Add(this->panelProjName);
 			this->panelMenu->Dock = System::Windows::Forms::DockStyle::Left;
 			this->panelMenu->Location = System::Drawing::Point(0, 0);
@@ -114,26 +128,82 @@ namespace airways {
 			this->panelMenu->Size = System::Drawing::Size(230, 681);
 			this->panelMenu->TabIndex = 0;
 			// 
+			// buttonTbd
+			// 
+			this->buttonTbd->FlatAppearance->BorderSize = 0;
+			this->buttonTbd->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->buttonTbd->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->buttonTbd->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->buttonTbd->Location = System::Drawing::Point(0, 258);
+			this->buttonTbd->Name = L"buttonTbd";
+			this->buttonTbd->Size = System::Drawing::Size(230, 79);
+			this->buttonTbd->TabIndex = 3;
+			this->buttonTbd->Text = L"tbd";
+			this->buttonTbd->UseVisualStyleBackColor = true;
+			// 
+			// buttonShowFlights
+			// 
+			this->buttonShowFlights->FlatAppearance->BorderSize = 0;
+			this->buttonShowFlights->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->buttonShowFlights->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->buttonShowFlights->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->buttonShowFlights->Location = System::Drawing::Point(0, 185);
+			this->buttonShowFlights->Name = L"buttonShowFlights";
+			this->buttonShowFlights->Size = System::Drawing::Size(230, 79);
+			this->buttonShowFlights->TabIndex = 2;
+			this->buttonShowFlights->Text = L"Flights list";
+			this->buttonShowFlights->UseVisualStyleBackColor = true;
+			// 
+			// buttonNewFlight
+			// 
+			this->buttonNewFlight->FlatAppearance->BorderSize = 0;
+			this->buttonNewFlight->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->buttonNewFlight->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->buttonNewFlight->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->buttonNewFlight->Location = System::Drawing::Point(0, 100);
+			this->buttonNewFlight->Name = L"buttonNewFlight";
+			this->buttonNewFlight->Size = System::Drawing::Size(230, 79);
+			this->buttonNewFlight->TabIndex = 1;
+			this->buttonNewFlight->Text = L"Create new flight";
+			this->buttonNewFlight->UseVisualStyleBackColor = true;
+			// 
 			// panelProjName
 			// 
 			this->panelProjName->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(75)), static_cast<System::Int32>(static_cast<System::Byte>(75)),
 				static_cast<System::Int32>(static_cast<System::Byte>(75)));
-			this->panelProjName->Controls->Add(this->pictureBox1);
+			this->panelProjName->Controls->Add(this->buttonHome);
 			this->panelProjName->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panelProjName->Location = System::Drawing::Point(0, 0);
 			this->panelProjName->Name = L"panelProjName";
-			this->panelProjName->Size = System::Drawing::Size(230, 103);
+			this->panelProjName->Size = System::Drawing::Size(230, 100);
 			this->panelProjName->TabIndex = 0;
 			// 
-			// pictureBox1
+			// buttonHome
 			// 
-			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(0, 0);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(230, 103);
-			this->pictureBox1->TabIndex = 0;
-			this->pictureBox1->TabStop = false;
+			this->buttonHome->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"buttonHome.BackgroundImage")));
+			this->buttonHome->FlatAppearance->BorderSize = 0;
+			this->buttonHome->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->buttonHome->Location = System::Drawing::Point(0, 0);
+			this->buttonHome->Name = L"buttonHome";
+			this->buttonHome->Size = System::Drawing::Size(230, 100);
+			this->buttonHome->TabIndex = 0;
+			this->buttonHome->UseVisualStyleBackColor = true;
+			// 
+			// pictureBoxBackground
+			// 
+			this->pictureBoxBackground->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBoxBackground.Image")));
+			this->pictureBoxBackground->Location = System::Drawing::Point(236, 100);
+			this->pictureBoxBackground->Name = L"pictureBoxBackground";
+			this->pictureBoxBackground->Size = System::Drawing::Size(1025, 581);
+			this->pictureBoxBackground->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBoxBackground->TabIndex = 1;
+			this->pictureBoxBackground->TabStop = false;
 			// 
 			// Form1
 			// 
@@ -142,12 +212,13 @@ namespace airways {
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(31)), static_cast<System::Int32>(static_cast<System::Byte>(31)),
 				static_cast<System::Int32>(static_cast<System::Byte>(31)));
 			this->ClientSize = System::Drawing::Size(1264, 681);
+			this->Controls->Add(this->pictureBoxBackground);
 			this->Controls->Add(this->panelMenu);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->panelMenu->ResumeLayout(false);
 			this->panelProjName->ResumeLayout(false);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxBackground))->EndInit();
 			this->ResumeLayout(false);
 
 		}
@@ -160,6 +231,7 @@ namespace airways {
 
 
 	private:
+
 
 
 
