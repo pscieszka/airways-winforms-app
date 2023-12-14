84d306581601944e9e7bf8c995ccd0b729c1d8c1
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-12-14 21:13:43 +0100

    more buttons

--- airways/airways/Map1.cpp
+++ airways/airways/Map1.cpp
@@ -8,7 +8,28 @@ System::Void airways::Map1::circleButton(System::Windows::Forms::Control^ contro
 	control->Region = gcnew System::Drawing::Region(path);
 }
 
-System::Void airways::Map1::button1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
+System::Void airways::Map1::Warsaw_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
 {
 	circleButton(this->Warsaw);
+}
+
+System::Void airways::Map1::Berlin_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
+{
+	circleButton(this->Berlin);
+}
+
+System::Void airways::Map1::Paris_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
+{
+	circleButton(this->Paris);
+}
+
+System::Void airways::Map1::Madrid_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
+{
+	circleButton(this->Madrid);
+}
+
+System::Void airways::Map1::London_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
+{
+	circleButton(this->London);
+
 }
--- airways/airways/Map1.h
+++ airways/airways/Map1.h
@@ -36,6 +36,16 @@ namespace airways {
 		}
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	private: System::Windows::Forms::Button^ Warsaw;
+	private: System::Windows::Forms::Button^ Berlin;
+	private: System::Windows::Forms::Button^ Paris;
+	private: System::Windows::Forms::Button^ Madrid;
+	private: System::Windows::Forms::Button^ London;
+
+
+
+
+
+
 
 
 	protected:
@@ -59,6 +69,10 @@ namespace airways {
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Map1::typeid));
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->Warsaw = (gcnew System::Windows::Forms::Button());
+			this->Berlin = (gcnew System::Windows::Forms::Button());
+			this->Paris = (gcnew System::Windows::Forms::Button());
+			this->Madrid = (gcnew System::Windows::Forms::Button());
+			this->London = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -77,18 +91,75 @@ namespace airways {
 				static_cast<System::Int32>(static_cast<System::Byte>(172)));
 			this->Warsaw->FlatAppearance->BorderSize = 0;
 			this->Warsaw->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->Warsaw->Location = System::Drawing::Point(638, 262);
+			this->Warsaw->Location = System::Drawing::Point(632, 268);
 			this->Warsaw->Name = L"Warsaw";
 			this->Warsaw->Size = System::Drawing::Size(25, 25);
 			this->Warsaw->TabIndex = 1;
+			this->Warsaw->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageBeforeText;
 			this->Warsaw->UseVisualStyleBackColor = false;
-			this->Warsaw->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Map1::button1_Paint);
+			this->Warsaw->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Map1::Warsaw_Paint);
+			// 
+			// Berlin
+			// 
+			this->Berlin->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(172)), static_cast<System::Int32>(static_cast<System::Byte>(172)),
+				static_cast<System::Int32>(static_cast<System::Byte>(172)));
+			this->Berlin->FlatAppearance->BorderSize = 0;
+			this->Berlin->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Berlin->Location = System::Drawing::Point(497, 284);
+			this->Berlin->Name = L"Berlin";
+			this->Berlin->Size = System::Drawing::Size(25, 25);
+			this->Berlin->TabIndex = 2;
+			this->Berlin->UseVisualStyleBackColor = false;
+			this->Berlin->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Map1::Berlin_Paint);
+			// 
+			// Paris
+			// 
+			this->Paris->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(172)), static_cast<System::Int32>(static_cast<System::Byte>(172)),
+				static_cast<System::Int32>(static_cast<System::Byte>(172)));
+			this->Paris->FlatAppearance->BorderSize = 0;
+			this->Paris->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Paris->Location = System::Drawing::Point(325, 403);
+			this->Paris->Name = L"Paris";
+			this->Paris->Size = System::Drawing::Size(25, 25);
+			this->Paris->TabIndex = 3;
+			this->Paris->UseVisualStyleBackColor = false;
+			this->Paris->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Map1::Paris_Paint);
+			// 
+			// Madrid
+			// 
+			this->Madrid->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(172)), static_cast<System::Int32>(static_cast<System::Byte>(172)),
+				static_cast<System::Int32>(static_cast<System::Byte>(172)));
+			this->Madrid->FlatAppearance->BorderSize = 0;
+			this->Madrid->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Madrid->Location = System::Drawing::Point(174, 581);
+			this->Madrid->Name = L"Madrid";
+			this->Madrid->Size = System::Drawing::Size(25, 25);
+			this->Madrid->TabIndex = 4;
+			this->Madrid->UseVisualStyleBackColor = false;
+			this->Madrid->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Map1::Madrid_Paint);
+			// 
+			// London
+			// 
+			this->London->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(172)), static_cast<System::Int32>(static_cast<System::Byte>(172)),
+				static_cast<System::Int32>(static_cast<System::Byte>(172)));
+			this->London->FlatAppearance->BorderSize = 0;
+			this->London->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->London->Location = System::Drawing::Point(263, 284);
+			this->London->Name = L"London";
+			this->London->Size = System::Drawing::Size(25, 25);
+			this->London->TabIndex = 5;
+			this->London->UseVisualStyleBackColor = false;
+			this->London->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Map1::London_Paint);
 			// 
 			// Map1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1039, 681);
+			this->Controls->Add(this->London);
+			this->Controls->Add(this->Madrid);
+			this->Controls->Add(this->Paris);
+			this->Controls->Add(this->Berlin);
 			this->Controls->Add(this->Warsaw);
 			this->Controls->Add(this->pictureBox1);
 			this->Name = L"Map1";
@@ -98,8 +169,13 @@ namespace airways {
 
 		}
 #pragma endregion
-	private: System::Void circleButton(System::Windows::Forms::Control^ control);
-	private: System::Void button1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e);
+	private: 
+	System::Void circleButton(System::Windows::Forms::Control^ control);
+	System::Void Warsaw_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e);
+    System::Void Berlin_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e);
+	System::Void Paris_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e);
+	System::Void Madrid_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e);
+	System::Void London_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e);
 	
 		
 	
