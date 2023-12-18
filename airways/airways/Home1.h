b8c78787e201667bfba54200a2cbb56f33597ea8
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-12-18 20:14:39 +0100

    home page done

--- airways/airways/Home1.h
+++ airways/airways/Home1.h
@@ -17,7 +17,23 @@ namespace airways {
 		Home1(void)
 		{
 			InitializeComponent();
+			System::Drawing::Point pos = label6->Parent->PointToScreen(label6->Location);
+			pos = background->PointToClient(pos);
+			label6->Parent = background;
+			label6->Location = pos;
+			label6->BackColor = System::Drawing::Color::Transparent;
 
+			System::Drawing::Point pos2 = label5->Parent->PointToScreen(label5->Location);
+			pos2 = background->PointToClient(pos2);
+			label5->Parent = background;
+			label5->Location = pos2;
+			label5->BackColor = System::Drawing::Color::Transparent;
+
+			System::Drawing::Point pos3 = label4->Parent->PointToScreen(label4->Location);
+			pos3 = background->PointToClient(pos3);
+			label4->Parent = background;
+			label4->Location = pos3;
+			label4->BackColor = System::Drawing::Color::Transparent;
 			//
 			
 
@@ -36,7 +52,20 @@ namespace airways {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ background;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::PictureBox^ pictureBox3;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::PictureBox^ pictureBox4;
+	private: System::Windows::Forms::Label^ label6;
+
+	protected:
+
 	protected:
 
 
@@ -107,36 +136,181 @@ namespace airways {
 		
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Home1::typeid));
+			this->background = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox4 = (gcnew System::Windows::Forms::PictureBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->background))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// background
+			// 
+			this->background->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"background.Image")));
+			this->background->Location = System::Drawing::Point(0, 0);
+			this->background->Name = L"background";
+			this->background->Size = System::Drawing::Size(1040, 720);
+			this->background->TabIndex = 0;
+			this->background->TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(110, 178);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(35, 35);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 54.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
-			this->label1->ForeColor = System::Drawing::Color::CornflowerBlue;
-			this->label1->Location = System::Drawing::Point(394, 282);
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->label1->Location = System::Drawing::Point(62, 27);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(135, 55);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"temp";
+			this->label1->Size = System::Drawing::Size(370, 83);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Welcome!";
 			// 
-			// Form1
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->label2->Location = System::Drawing::Point(105, 116);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(225, 33);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Getting Started";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->label3->Location = System::Drawing::Point(166, 188);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(190, 25);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"Create new flight";
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(111, 244);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(35, 35);
+			this->pictureBox2->TabIndex = 5;
+			this->pictureBox2->TabStop = false;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label4->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->label4->Location = System::Drawing::Point(166, 254);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(420, 25);
+			this->label4->TabIndex = 6;
+			this->label4->Text = L"Choose two cities to create connection";
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.Image")));
+			this->pictureBox3->Location = System::Drawing::Point(110, 315);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(35, 35);
+			this->pictureBox3->TabIndex = 7;
+			this->pictureBox3->TabStop = false;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label5->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->label5->Location = System::Drawing::Point(166, 325);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(392, 25);
+			this->label5->TabIndex = 8;
+			this->label5->Text = L"Add passengers and their baggages";
+			// 
+			// pictureBox4
+			// 
+			this->pictureBox4->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox4.Image")));
+			this->pictureBox4->Location = System::Drawing::Point(111, 388);
+			this->pictureBox4->Name = L"pictureBox4";
+			this->pictureBox4->Size = System::Drawing::Size(35, 35);
+			this->pictureBox4->TabIndex = 9;
+			this->pictureBox4->TabStop = false;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->BackColor = System::Drawing::Color::Transparent;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label6->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(153)), static_cast<System::Int32>(static_cast<System::Byte>(153)),
+				static_cast<System::Int32>(static_cast<System::Byte>(153)));
+			this->label6->Location = System::Drawing::Point(166, 398);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(202, 25);
+			this->label6->TabIndex = 10;
+			this->label6->Text = L"Manage your data";
+			// 
+			// Home1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(31)), static_cast<System::Int32>(static_cast<System::Byte>(31)),
 				static_cast<System::Int32>(static_cast<System::Byte>(31)));
 			this->ClientSize = System::Drawing::Size(1039, 681);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->pictureBox4);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->pictureBox3);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->background);
 			this->Name = L"Home1";
 			this->Text = L"Home1";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->background))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
+
+
 
 
 };
